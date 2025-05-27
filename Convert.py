48428b4817a1d6b553e8a9dad1e3f91c2c2ceed0
LeoWiid <139720063+LeoWiid@users.noreply.github.com>
2025-05-28 11:04:56 +1200

    Add files via upload

---
+++ Convert.py
@@ -0,0 +1,24 @@
+import conversion_rounding as cr  # make sure this file exists with to_AUD and to_USD functions
+
+def convert(question):
+    convert_to_usd = 1
+
+    while True:
+        try:
+            to_convert = float(input(question))
+            break
+        except ValueError:
+            print("Please enter a valid number (digits only).")
+
+    if convert_to_usd:
+        answer = cr.to_USD(to_convert)
+        answer_statement = f"{to_convert} NZD is {answer} USD"
+    else:
+        answer = cr.to_AUD(to_convert)
+        answer_statement = f"{to_convert} NZD is {answer} AUD"
+
+    return answer_statement
+
+# Main program
+answer2 = convert("Enter a number to convert: ")
+print(f"Conversion result: {answer2}")
